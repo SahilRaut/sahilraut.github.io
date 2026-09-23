cdb1216f28021665cca6f3d24a35fd8aa3a38744
Sahil Raut <sahilvraut777@gmail.com>
2026-09-24 01:36:08 +0530

    Redesign portfolio as pixel-art night-city skyline theme

--- script.js
+++ script.js
@@ -4,10 +4,3 @@ document.querySelectorAll('a[href^="#"]').forEach(link=>{
     if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth'});}
   });
 });
-
-setInterval(()=>{
-  if(Math.random()>0.94){
-    document.body.style.filter='brightness(1.4) contrast(1.1)';
-    setTimeout(()=>{document.body.style.filter='';},70);
-  }
-},1500);
